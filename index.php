8eb1c15e828552d63799ecacba3ed4702d2f5475
Jarret Long <500153707@fvtc.edu>
2018-01-31 20:47:21 -0600

    Assignment #2

---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Jarret's Website</title>
+    <link rel="stylesheet" type="text/css" href="css/base.css">
+</head>
+<body>
+    <header><h1>Jarret's Website</h1></header>
+    <nav>
+        <ul>
+            <li><a href="/">Homepage</a></li>
+            <li><a href="#">Loop</a></li>
+            <li><a href="#">Countdown</a></li>
+        </ul>
+    </nav>
+    <main>
+        <img src="images/jarret.jpg" alt="Picture of Jarret"/>
+        <p>My name is Jarret Long, I am a software development student looking to better my coding in any way possible. I currently work at Panera Bread as a shift supervisor. Some hobbies include, playing video games, watching movies/tv shows, and watching football.</p>
+    </main>
+    <footer>&copy; 2018 JarretLong.com</footer>
+</body>
+
+
+
+</html>
